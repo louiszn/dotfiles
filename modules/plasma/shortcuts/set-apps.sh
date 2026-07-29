e68e0de5a60a3000e04ef3518540f29b2c4b0308
Louis <git@louiszn.fyi>
2026-07-29 15:30:11 +0700

    feat: make krunner use Meta+Space

--- modules/plasma/shortcuts/set-apps.sh
+++ modules/plasma/shortcuts/set-apps.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 source "$(dirname "$0")/functions.sh"
 
 bind_app "org.kde.konsole.desktop" "Meta+T"
-bind_app "org.kde.krunner.desktop" "Meta+R"
+bind_app "org.kde.krunner.desktop" "Meta+R" "Meta+Space"
 bind_app "org.kde.dolphin.desktop" "Meta+E"
